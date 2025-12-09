966db34844d526c50bdd48a1912a056dfd72cdfc
saikat-pundit <saikatpundit@yandex.com>
2025-12-09 19:51:30 +0530

    Update nifty50_top10.py

--- nifty50_top10.py
+++ nifty50_top10.py
@@ -10,18 +10,31 @@ url = "https://www.nseindia.com/api/equity-stockIndices?index=NIFTY%2050"
 response = requests.get(url, headers=headers)
 data = response.json()
 
-records = []
+target_symbols = [
+    "RELIANCE",
+    "HDFCBANK", 
+    "BHARTIARTL",
+    "TCS",
+    "ICICIBANK",
+    "SBIN",
+    "INFY",
+    "BAJFINANCE",
+    "LT",
+    "HINDUNILVR"
+]
+
+symbol_dict = {}
 for item in data['data']:
     symbol = item.get('symbol')
     
-    if symbol != "NIFTY 50":
+    if symbol in target_symbols:
         pchange = item.get('pChange')
         if pchange is not None:
             percent_change_str = f"{pchange}%"
         else:
             percent_change_str = ""
         
-        records.append({
+        symbol_dict[symbol] = {
             'Symbol': symbol,
             'Last': item.get('lastPrice'),
             'Change': item.get('change'),
@@ -29,7 +42,12 @@ for item in data['data']:
             'Previous Close': item.get('previousClose'),
             'Year High': item.get('yearHigh'),
             'Year Low': item.get('yearLow')
-        })
+        }
+
+records = []
+for symbol in target_symbols:
+    if symbol in symbol_dict:
+        records.append(symbol_dict[symbol])
 
 df = pd.DataFrame(records)
 df.to_csv('nifty50_stocks_top10.csv', index=False)
