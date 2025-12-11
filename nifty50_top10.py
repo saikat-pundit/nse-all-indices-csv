ffc3989d91ee62ce20fbcce5953812e74e439d20
saikat-pundit <saikatpundit@yandex.com>
2025-12-11 18:45:33 +0530

    Update nifty50_top10.py

--- nifty50_top10.py
+++ nifty50_top10.py
@@ -52,7 +52,7 @@ for symbol in target_symbols:
         records.append(symbol_dict[symbol])
 os.makedirs('../Data', exist_ok=True)
 df = pd.DataFrame(records)
-filename = '../Data/nifty50_stocks_top10.csv'
+filename = 'Data/nifty50_stocks_top10.csv'
 df.to_csv(filename, index=False)
 
 # Add timestamp row
